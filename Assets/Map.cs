d6cac64c47019cb03210943576082071a795fcb7
Giovana Melo <giovanamelomr@gmail.com>
2022-03-28 09:03:33 -0300

    First commit

---
+++ Assets/Map.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    private Vector3 _mousePosition;
+    private readonly float _speed = 10f;
+    [SerializeField]
+    private Rigidbody rb;
+    private void Update()
+    {
+        if (Camera.main != null) _mousePosition = Camera.main.ViewportToWorldPoint(Input.mousePosition);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        ProcessInput(horizontal, vertical);
+    }
+
+    private void ProcessInput(float horizontal, float vertical)
+    {
+        transform.Rotate(new Vector3(0, 0, -1), _speed * horizontal * Time.deltaTime);
+        transform.Rotate(new Vector3(-1, 0 ,0), _speed * vertical* Time.deltaTime);
+
+       // if (vertical == 0 || horizontal == 0) return;
+        
+       
+        
+
+    }
+}
